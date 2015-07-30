b560d82a50c8462bccd348e1b4277e77b036bcd9
Sendy Halim <sendyhalim93@gmail.com>
2015-07-30 22:25:43 +0700

    Add conditional for creating links in linux

--- create_ln.sh
+++ create_ln.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+PWD=$(pwd)
 
-ln -s $(pwd)/gitignore ~/.gitignore
-ln -s $(pwd)/gitconfig ~/.gitconfig
-ln -s $(pwd)/osx/slate ~/.slate
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $PWD/gitignore ~/.gitignore
+ln -s $PWD/gitconfig ~/.gitconfig
+
+if [[ $(uname) == "Linux" ]]; then
+  echo "Creating links on Linux"
+  ln -s $PWD/linux/tmux.conf ~/.tmux.conf
+else
+  echo "Creating links on OSX"
+  ln -s $PWD/osx/slate ~/.slate
+  ln -s $PWD/osx/tmux.conf ~/.tmux.conf
+fi
