2eb889e41b503c5da93251256655329cc1e5309c
Sendy Halim <sendyhalim93@gmail.com>
2017-03-09 11:49:33 +0700

    Set color for double quoted argument

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -1,8 +1,14 @@
 # ZSH Styles
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md
+# Declare the variable
+typeset -A ZSH_HIGHLIGHT_STYLES
+
 ZSH_HIGHLIGHT_STYLES[command]=fg=39
 ZSH_HIGHLIGHT_STYLES[builtin]=fg=39
 ZSH_HIGHLIGHT_STYLES[alias]=fg=106
 ZSH_HIGHLIGHT_STYLES[globbing]=fg=140
 ZSH_HIGHLIGHT_STYLES[path]=fg=102
 ZSH_HIGHLIGHT_STYLES[comment]=fg=244
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=184
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=184
 
