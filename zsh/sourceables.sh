ff29a02a79bf0c4ae40208813e3afa9357c8982a
Sendy Halim <sendyhalim93@gmail.com>
2017-10-30 00:56:30 +0700

    Replace zshmark with fzfmark

--- zsh/sourceables.sh
+++ zsh/sourceables.sh
@@ -1,3 +1,3 @@
-source /Users/sendyhalim/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-jocelynmallon-SLASH-zshmarks/zshmarks.plugin.zsh
 source /Users/sendyhalim/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 source /Users/sendyhalim/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source /Users/sendyhalim/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-uvaes-SLASH-fzf-marks/fzf-marks.plugin.zsh
