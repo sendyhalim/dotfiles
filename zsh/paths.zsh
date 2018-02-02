ff8cc320cd721f579d475f41fbd4b9ba88bde955
Sendy Halim <sendyhalim93@gmail.com>
2018-02-02 22:13:35 +0700

    Append ~/.cabal/bin and ~/.local/bin in paths.zsh

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,4 +1,7 @@
 # Path
 mkdir -p $HOME/.local/bin
+export PATH=/usr/local/sbin:$PATH
+export PATH=/usr/local/bin:$PATH
+export PATH=$PATH:$HOME/.cabal/bin
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.composer/vendor/bin
