403fa81cf8b1546555ae51f015584d70bb741d52
Sendy Halim <sendyhalim93@gmail.com>
2018-03-08 00:33:30 +0700

    Do prepend operation to $PATH instead of appending it

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -2,6 +2,6 @@
 mkdir -p $HOME/.local/bin
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
-export PATH=$PATH:$HOME/.cabal/bin
-export PATH=$PATH:$HOME/.local/bin
-export PATH=$PATH:$HOME/.composer/vendor/bin
+export PATH=$HOME/.composer/vendor/bin:$PATH
+export PATH=$HOME/.cabal/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
