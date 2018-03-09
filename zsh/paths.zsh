2da69bdd39aeb26c25faaac9e1b73fc17d4b6e9a
Sendy Halim <sendyhalim93@gmail.com>
2018-03-09 11:04:37 +0700

    Reorder $PATH priorities

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -2,6 +2,6 @@
 mkdir -p $HOME/.local/bin
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
-export PATH=$HOME/.composer/vendor/bin:$PATH
-export PATH=$HOME/.cabal/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.cabal/bin:$PATH
+export PATH=$HOME/.composer/vendor/bin:$PATH
