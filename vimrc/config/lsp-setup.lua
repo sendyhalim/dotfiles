ab95d9de8499865c6c485455428144f35c5ee57c
Sendy Halim <sendyhalim93@gmail.com>
2022-09-08 23:57:50 +0700

    fix(vim): Fix setup for vim filepath completion

--- vimrc/config/lsp-setup.lua
+++ vimrc/config/lsp-setup.lua
@@ -45,24 +45,9 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'ultisnips' },
     { name = 'buffer' },
+    { name = 'path' },
   },
 }
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
 
 
 ---------------------------------
